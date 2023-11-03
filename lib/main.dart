1d0ae317eee05b651acc718b89f4ad935952ef5c
Aleksandr Bekrenev <praise2001@yandex.ru>
2023-11-03 22:47:38 +0700

    feat(): routing at app start

--- lib/main.dart
+++ lib/main.dart
@@ -3,6 +3,7 @@ import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 
 import 'package:random_song/domain/di.dart';
+import 'package:random_song/scenes/onboarding/onboarding_screen.dart';
 import 'package:random_song/scenes/request_random_song/cubit/request_random_song_cubit.dart';
 import 'package:random_song/scenes/request_random_song/request_random_song_page.dart';
 
@@ -20,9 +21,24 @@ class MainApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       routes: {
-        Routes.initialRoute: (context) => _getRequestRandomSongPage(),
+        Routes.onboardingRoute: (context) => const OnboardingScreen(),
+        Routes.randomSongRoute: (context) => _getRequestRandomSongPage(),
       },
-      initialRoute: Routes.initialRoute,
+      home: FutureBuilder (
+        future: getInitialRoute(),
+        builder: (context, snapshot) {
+          if (snapshot.hasData) {
+            Future.microtask(() => Navigator.of(context).pushReplacementNamed(snapshot.data!));
+            return Container();
+          } else {
+            return const SizedBox(
+              width: 60,
+              height: 60,
+              child: CircularProgressIndicator(),
+            );
+          }
+        }
+      )
     );
   }
 
@@ -34,8 +50,13 @@ class MainApp extends StatelessWidget {
       child: const RequestRandomSongPage(),
     );
   }
+
+  Future<String> getInitialRoute() async {
+    return await _di.firstLaunchService.isFirstLaunch ? Routes.onboardingRoute : Routes.randomSongRoute;
+  }
 }
 
 class Routes {
-  static const initialRoute = '/';
+  static const onboardingRoute = '/onboarding';
+  static const randomSongRoute = '/random-song';
 }
