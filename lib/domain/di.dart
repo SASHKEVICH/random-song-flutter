1b8c035f584205449f4f55c8ebcd95c4cff0f7a6
Aleksandr Bekrenev <praise2001@yandex.ru>
2023-11-04 13:06:19 +0700

    feat(): song view model factory in di

--- lib/domain/di.dart
+++ lib/domain/di.dart
@@ -1,15 +1,21 @@
+import 'package:random_song/domain/factories/random_song_view_model_factory.dart';
 import 'package:random_song/domain/services/first_launch_service.dart';
 import 'package:random_song/domain/services/random_song_service.dart';
 
 class Di {
   RandomSongService? _randomSongService;
   FirstLaunchService? _firstLaunchService;
+  RandomSongViewModelFactory? _randomSongViewModelFactory;
 
   RandomSongService get randomSongService {
-    return _randomSongService != null ? _randomSongService! : RandomSongService();
+    return _randomSongService ?? RandomSongService();
   }
 
   FirstLaunchService get firstLaunchService {
-    return _firstLaunchService != null ? _firstLaunchService! : FirstLaunchService();
+    return _firstLaunchService ?? FirstLaunchService();
+  }
+
+  RandomSongViewModelFactory get randomSongViewModelFactory {
+    return _randomSongViewModelFactory ?? RandomSongViewModelFactory();
   }
 }
