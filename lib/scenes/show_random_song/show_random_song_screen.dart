c0973bc538569011183400d228a6071f6b29a261
Aleksandr Bekrenev <praise2001@yandex.ru>
2023-11-11 15:29:57 +0700

    feat(): making random song screen beautyful :3

--- lib/scenes/show_random_song/show_random_song_screen.dart
+++ lib/scenes/show_random_song/show_random_song_screen.dart
@@ -15,41 +15,84 @@ class _ShowRandomSongScreenState extends State<ShowRandomSongScreen> {
   @override
   Widget build(BuildContext context) {
     final song = ModalRoute.of(context)?.settings.arguments as RandomSongViewModel;
+    final screenWidth = MediaQuery.of(context).size.width - 2 * _Constants.defaultPadding;
 
     return Scaffold(
       appBar: AppBar(
         backgroundColor: Colors.red,
         title: Text(song.songTitle),
       ),
-      body: Column(
-        crossAxisAlignment: CrossAxisAlignment.center,
-        children: [
-          Image(image: NetworkImage(song.songArtImageUrl)),
-          Text("Название песни: ${song.songTitle}"),
-          Text("Исполнитель: ${song.artists}"),
-          Text("Дата выхода: ${song.releaseDate}"),
-          ElevatedButton(
-            onPressed: () {
-              Navigator.of(context).pushNamed(
-                Routes.showSongLyricsRoute,
-                arguments: song
-              );
-            },
-            child: const Text("Текст песни")
+      body: SafeArea(
+        child: Padding(
+          padding: const EdgeInsets.all(_Constants.defaultPadding),
+          child: SingleChildScrollView(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                SizedBox(
+                  height: screenWidth,
+                  width: screenWidth,
+                  child: ClipRRect(
+                    borderRadius: BorderRadius.circular(20),
+                    child: Image.network(song.songArtImageUrl,)
+                  ),
+                ),
+                const SizedBox(height: 15,),
+                SizedBox(
+                  width: screenWidth,
+                  child: Wrap(
+                    direction: Axis.vertical,
+                    spacing: 15,
+                    alignment: WrapAlignment.start,
+                    children: [
+                      Text("Название песни: ${song.songTitle}"),
+                      Text("Исполнитель: ${song.artists}"),
+                      Text("Дата выхода: ${song.releaseDate ?? "нет информации"}"),
+                    ],
+                  ),
+                ),
+                const SizedBox(height: 15,),
+                ElevatedButton(
+                  onPressed: () {
+                    Navigator.of(context).pushNamed(
+                      Routes.showSongLyricsRoute,
+                      arguments: song
+                    );
+                  },
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: Colors.red,
+                    fixedSize: const Size(_Constants.defaultButtonWidth, _Constants.defaultButtonHeight)
+                  ),
+                  child: const Text(_Constants.lyricsButtonTitle)
+                ),
+                ElevatedButton(
+                  onPressed: () => _launchUrl(song.youtubeUrl),
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: Colors.red,
+                    fixedSize: const Size(_Constants.defaultButtonWidth, _Constants.defaultButtonHeight)
+                  ),
+                  child: const Text(_Constants.youtubeButtonTitle),
+                ),
+                ElevatedButton(
+                  onPressed: () => _launchUrl(song.soundcloudUrl),
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: Colors.red,
+                    fixedSize: const Size(_Constants.defaultButtonWidth, _Constants.defaultButtonHeight)
+                  ),
+                  child: const Text(_Constants.soundcloudButtonTitle),
+                ),
+                ElevatedButton(
+                  onPressed: () => _launchUrl(song.spotifyUrl),
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: Colors.red,
+                    fixedSize: const Size(_Constants.defaultButtonWidth, _Constants.defaultButtonHeight)
+                  ),
+                  child: const Text(_Constants.spotifyButtonTitle),
+                )
+              ],
+            ),
           ),
-          ElevatedButton(
-            onPressed: () => _launchUrl(song.youtubeUrl),
-            child: const Text("YouTube"),
-          ),
-          ElevatedButton(
-            onPressed: () => _launchUrl(song.soundcloudUrl),
-            child: const Text("Soundcloud"),
-          ),
-          ElevatedButton(
-            onPressed: () => _launchUrl(song.spotifyUrl),
-            child: const Text("Spotify"),
-          )
-        ],
+        )
       )
     );
   }
@@ -62,8 +105,8 @@ class _ShowRandomSongScreenState extends State<ShowRandomSongScreen> {
       );
 
       AlertDialog alert = AlertDialog(
-        title: const Text("Нет ссылки на ресурс"),
-        content: const Text("К сожалению, открыть песню по этой ссылке не получится :("),
+        title: const Text(_Constants.cannotOpenSongAlertTitle),
+        content: const Text(_Constants.cannotOpenSongAlertMessage),
         actions: [
           okButton,
         ],
@@ -84,4 +127,19 @@ class _ShowRandomSongScreenState extends State<ShowRandomSongScreen> {
       throw Exception('Could not launch $url');
     }
   }
+}
+
+class _Constants {
+  static const String youtubeButtonTitle = "Открыть на YouTube";
+  static const String soundcloudButtonTitle = "Прослушать на Soundcloud";
+  static const String spotifyButtonTitle = "Открыть в Spotify";
+  static const String lyricsButtonTitle = "Текст песни";
+
+  static const String cannotOpenSongAlertTitle = "Нет ссылки на ресурс";
+  static const String cannotOpenSongAlertMessage = "К сожалению, открыть песню по этой ссылке не получится :(";
+
+  static const double defaultPadding = 10;
+
+  static const double defaultButtonWidth = 250;
+  static const double defaultButtonHeight = 40;
 }
